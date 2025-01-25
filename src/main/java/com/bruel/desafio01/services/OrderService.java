381fa386b3d62c56777d7fcd1f73b8e5205f26ea
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-01-25 01:37:04 -0300

    criação dos services

--- src/main/java/com/bruel/desafio01/services/OrderService.java
+++ src/main/java/com/bruel/desafio01/services/OrderService.java
@@ -1,4 +1,15 @@
 package com.bruel.desafio01.services;
 
+import com.bruel.desafio01.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Order order){
+        return order.getBasic() - (order.getBasic()*(order.getDiscount()/100)) + shippingService.shipment(order);
+    }
 }
--- src/main/java/com/bruel/desafio01/services/ShippingService.java
+++ src/main/java/com/bruel/desafio01/services/ShippingService.java
@@ -1,4 +1,21 @@
 package com.bruel.desafio01.services;
 
+import com.bruel.desafio01.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShippingService {
+    public double shipment(Order order){
+        Double basic = order.getBasic();
+
+        if(basic < 100){
+            return 20.0;
+        } else {
+            if( basic < 200){
+                return 12.0;
+            } else {
+                return 0.0;
+            }
+        }
+    }
 }
